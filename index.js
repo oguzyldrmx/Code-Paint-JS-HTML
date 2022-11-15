df40d1d8bb0ddf770d83d85770c055a4859acaa4
Oğuz Yıldırım <oguzyldrm52@gmail.com>
2022-11-15 16:39:16 +0300

    Finalize

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const canvas = document.querySelector('#canvas');
 const ctx = canvas.getContext('2d');
 const drawColor = document.querySelector('#color');
-const lineWidthInput = document.querySelector('lineWidth');
+const lineWidthInput = document.querySelector('#lineWidth');
 
 canvas.width = canvas.offsetWidth;
 canvas.height = canvas.offsetHeight;
@@ -17,13 +17,27 @@ document.addEventListener("mousedown",(e) =>{
 } ) 
 
 //Stop draw
+document.addEventListener("mouseup",() => {
+    document.removeEventListener("mousemove", draw);
+})
+
+//change line width and color
 let color = "black";
 let lineWidth = 5;
 
+document.addEventListener("change", () => {
+    color = drawColor.value;
+
+})
+document.addEventListener("change", () => {
+    lineWidth = lineWidthInput.value;
+    
+})
 function reposition(e) {
     position.x = e.clientX - canvas.offsetLeft;
     position.y = e.clientY - canvas.offsetTop;
 }
+//Render all
 function draw(e){
     ctx.beginPath();
     ctx.lineWidth = lineWidth;
@@ -35,3 +49,9 @@ function draw(e){
     ctx.stroke();
     ctx.closePath();
 }
+// clear button
+const clearBtn = document.querySelector('#clearBtn');
+
+clearBtn.addEventListener("click" , ()=>{
+    ctx.clearRect(0, 0, canvas.width , canvas.height);
+})
